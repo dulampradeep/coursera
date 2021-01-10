3d3907ba2c90ae41ddc1e33e668976731edb1cb5
pradeep <42362123+dulampradeep@users.noreply.github.com>
2021-01-10 23:06:51 +0530

    Add files via upload

---
+++ apps/PostboxAdmin/app/src/main/java/com/example/postboxadmin/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.postboxadmin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+    }
+}
